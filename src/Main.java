a388ed4eb64a8f8325a76c95bc55d6428160b133
Olexander Zhurba <starkyzh@gmail.com>
2023-04-28 13:35:58 +0200

    fill Map and solve task

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         /*
@@ -6,7 +10,32 @@ public class Main {
         Добавьте ещё один enum, который будет описывать времена года (зима, лето, весна, осень).
         Для введённого пользователем месяца напишите, к какому именно времени года относится выбранный месяц.
          */
+        Map<Month, Integer> daysPerMonth = new HashMap<>();
+        daysPerMonth.put(Month.JANUARY, 31);
+        daysPerMonth.put(Month.FEBRUARY, 28); // год не високосный
+        daysPerMonth.put(Month.MARCH, 31);
+        daysPerMonth.put(Month.APRIL, 30);
+        daysPerMonth.put(Month.MAY, 31);
+        daysPerMonth.put(Month.JUNE, 30);
+        daysPerMonth.put(Month.JULY, 31);
+        daysPerMonth.put(Month.AUGUST, 31);
+        daysPerMonth.put(Month.SEPTEMBER, 30);
+        daysPerMonth.put(Month.OCTOBER, 31);
+        daysPerMonth.put(Month.NOVEMBER, 30);
+        daysPerMonth.put(Month.DECEMBER, 31);
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the month number: ");
+        int monthNumber = scanner.nextInt(); // порядковый номер месяца
+        int monthIndex = monthNumber - 1; // индекс месяца в перечислении
+        Month monthKey = Month.values()[monthIndex]; // месяц "как ключ словаря"
+        int seasonIndex = monthNumber / 3;//
+        Season seasonKey = Season.values()[seasonIndex]; // месяц "как ключ словаря"
+        String seasonName = seasonKey.toString();
+        int dayInSelectedMonth = daysPerMonth.get(monthKey);
+        System.out.println(monthKey + " has " + dayInSelectedMonth + " days and season's name is " + seasonName);
+        System.out.println();
     }
 
     private enum Month {
