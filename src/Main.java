a0074ec9e3063b4754d7e671d93e736eddcce55a
Olexander Zhurba <starkyzh@gmail.com>
2023-04-28 13:31:01 +0200

    added enum Month

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,21 @@ public class Main {
         Добавьте ещё один enum, который будет описывать времена года (зима, лето, весна, осень).
         Для введённого пользователем месяца напишите, к какому именно времени года относится выбранный месяц.
          */
+
+    }
+
+    private enum Month {
+        JANUARY, // 0
+        FEBRUARY,
+        MARCH,
+        APRIL,
+        MAY,
+        JUNE,
+        JULY,
+        AUGUST,
+        SEPTEMBER,
+        OCTOBER,
+        NOVEMBER,
+        DECEMBER,
     }
 }
